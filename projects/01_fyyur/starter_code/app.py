8c07fdace126aa7bb28248a46163d705ffb691b0
Danil.Martakov <dsm013@gmail.com>
2020-03-23 10:08:20 -0500

    whitespace

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -33,33 +33,31 @@ migrate = Migrate(app, db)
 
 
 class Venue(db.Model):
-        __tablename__ = 'venues'
+    __tablename__ = 'venues'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
 
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String)
-        city = db.Column(db.String(120))
-        state = db.Column(db.String(120))
-        address = db.Column(db.String(120))
-        phone = db.Column(db.String(120))
-        image_link = db.Column(db.String(500))
-        facebook_link = db.Column(db.String(120))
-
-        # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
 class Artist(db.Model):
-        __tablename__ = 'artists'
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
 
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String)
-        city = db.Column(db.String(120))
-        state = db.Column(db.String(120))
-        phone = db.Column(db.String(120))
-        genres = db.Column(db.String(120))
-        image_link = db.Column(db.String(500))
-        facebook_link = db.Column(db.String(120))
-
-        # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 

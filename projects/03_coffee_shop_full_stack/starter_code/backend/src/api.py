d216b5b47bb43e55810a1e0dd2acf2e7b056a566
Danil.Martakov <dsm013@gmail.com>
2020-04-26 18:21:51 -0500

    Added get_drinks_detail

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -41,7 +41,7 @@ def get_drinks():
 
 
 '''
-@TODO implement endpoint
+implemented endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
@@ -49,6 +49,16 @@ def get_drinks():
         where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail():
+    drinks = Drink.query.all()
+    formatted_drinks = [drink.long() for drink in drinks]
+
+    return jsonify({
+        "success": True,
+        "drinks": formatted_drinks
+    }), 200
 
 
 '''
